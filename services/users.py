794a46e78f88cd70a596a7873013edb6957bb152
Philamand <contact@autobacklog.com>
2026-04-16 11:18:39 +0200

    Fixed create_user

--- services/users.py
+++ services/users.py
@@ -11,7 +11,7 @@ class UserService:
 
     async def create_user(self, user: UserFormData) -> str:
         """Create a new user and return the user's ID."""
-        result = await self.conn.execute(
+        result = await self.conn.fetch(
             "INSERT INTO users (qbit_host, qbit_port, qbit_user, qbit_pass, c411_key, torr9_key, lacale_key) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id",
             user.qbit_host,
             user.qbit_port,
@@ -21,4 +21,4 @@ class UserService:
             user.torr9_key,
             user.lacale_key,
         )
-        return result
+        return result[0]["id"]
