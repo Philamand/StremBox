213aa1946311ac2c4e2d6bf42ed20a61343f05e9
Philamand <contact@autobacklog.com>
2026-04-16 10:05:37 +0200

    Added simple template

--- routers/stremio.py
+++ routers/stremio.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 from constants import MANIFEST
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
 @router.get("/manifest.json")
