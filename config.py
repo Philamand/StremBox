4082d81c75bb4b95d63e0384f62e7bdad9d04f9f
Philamand <contact@autobacklog.com>
2026-04-17 10:43:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ QBT_HOST = os.getenv("QBT_HOST", "localhost")
 QBT_PORT = int(os.getenv("QBT_PORT", "8080"))
 QBT_USERNAME = os.getenv("QBT_USERNAME", "admin")
 QBT_PASSWORD = os.getenv("QBT_PASSWORD", "password")
+
+C411_API_KEY = os.getenv("C411_API_KEY", None)
