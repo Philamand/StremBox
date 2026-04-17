477a57f3fb86716527ed4bd2dbd41f61516c4677
Philamand <contact@autobacklog.com>
2026-04-17 10:43:40 +0200

    Update torrent_manager.py

--- services/torrent_manager.py
+++ services/torrent_manager.py
@@ -2,8 +2,9 @@ import time
 
 import qbittorrentapi
 from fastapi import HTTPException
+from qbittorrentapi import TorrentFilesList
 
-from config import QBT_HOST, QBT_PASSWORD, QBT_PORT, QBT_USERNAME
+from config import C411_API_KEY, QBT_HOST, QBT_PASSWORD, QBT_PORT, QBT_USERNAME
 
 
 class TorrentManager:
@@ -14,15 +15,27 @@ class TorrentManager:
         self._client = qbittorrentapi.Client(
             host=QBT_HOST, port=QBT_PORT, username=QBT_USERNAME, password=QBT_PASSWORD
         )
+        self.c411_api_key = C411_API_KEY
 
-    def add_torrent(self, download_link: str):
+    def add_torrent(self, hash: str, tracker: str):
         """Add a torrent URL or magnet link and enable sequential download."""
+        if tracker == "c411":
+            if not self.c411_api_key:
+                raise HTTPException(
+                    status_code=400, detail="Clé API C411 non configurée"
+                )
+            download_link = (
+                f"https://c411.org/api?t=get&id={hash}&apikey={self.c411_api_key}"
+            )
+        else:
+            raise HTTPException(status_code=400, detail="Tracker non supporté")
+
         return self._client.torrents_add(
             urls=download_link,
             is_sequential_download=True,
         )
 
-    def check_torrent(self, hash: str):
+    def check_torrent(self, hash: str) -> bool:
         """Check if a torrent is still active."""
         torrents = self._client.torrents_info(torrent_hashes=hash)
         return torrents != []
@@ -43,6 +56,6 @@ class TorrentManager:
             detail=f"Timed out after {int(timeout)}s waiting for torrent to be added.",
         )
 
-    def get_files(self, hash: str):
+    def get_files(self, hash: str) -> TorrentFilesList:
         """Return file entries for a given torrent hash."""
         return self._client.torrents_files(hash=hash)
