5acccd4850952084f0e21c101b24400cbe6bcff7
Philamand <contact@autobacklog.com>
2026-04-17 10:54:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ QBT_USERNAME = os.getenv("QBT_USERNAME", "admin")
 QBT_PASSWORD = os.getenv("QBT_PASSWORD", "password")
 
 C411_API_KEY = os.getenv("C411_API_KEY", None)
+
+BASE_DIR = os.getenv("BASE_DIR", "./data/")
