5cf2b9621ee416026a96cc406e1db41df1143de8
Philamand <contact@autobacklog.com>
2026-04-22 10:32:34 +0200

    Add cors

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.streams import router
 from routers.torrents import router as torrents_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router, prefix="/streams")
 app.include_router(torrents_router, prefix="/torrents")
