bde4e4c70967b2d68f6bb49cefb73e1f29223471
Philamand <contact@autobacklog.com>
2026-04-25 15:01:25 +0200

    Dashboard route now show torrents

--- routers/dashboard.py
+++ routers/dashboard.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from config import HANKO_URL
+from services.torrent_manager import TorrentManager
 from utils.auth import get_user
 
 dashboard_router = APIRouter()
@@ -10,9 +11,15 @@ templates = Jinja2Templates(directory="templates")
 
 
 @dashboard_router.get("/")
-async def dashboard(request: Request, user: str = Depends(get_user)) -> HTMLResponse:
+async def dashboard(
+    request: Request,
+    user: str = Depends(get_user),
+    torrent_manager: TorrentManager = Depends(),
+) -> HTMLResponse:
+    torrent_list = await torrent_manager.get_torrent_list()
+
     return templates.TemplateResponse(
         request=request,
         name="dashboard.html",
-        context={"hanko_url": HANKO_URL, "user": user},
+        context={"hanko_url": HANKO_URL, "user": user, "torrent_list": torrent_list},
     )
