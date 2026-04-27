06abd2517c1050ee564cd942ecf7043303a145af
Philamand <contact@autobacklog.com>
2026-04-27 12:24:18 +0200

    Use htmx for folder navigation

--- routers/files.py
+++ routers/files.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -5,6 +7,7 @@ from fastapi.templating import Jinja2Templates
 from config import HANKO_URL
 from services.files import FileManager
 from utils.auth import get_user
+from utils.htmx import is_htmx_request
 from utils.jinja_filters import register_filters
 
 router = APIRouter()
@@ -15,14 +18,19 @@ register_filters(templates.env)
 @router.get("/")
 async def list_files(
     request: Request,
+    is_htmx: Annotated[bool, Depends(is_htmx_request)],
+    file_manager: Annotated[FileManager, Depends()],
     folder: str | None = None,
     user: str = Depends(get_user),
-    file_manager: FileManager = Depends(),
 ) -> HTMLResponse:
     """Return the list of files from the configured directory."""
     files = await file_manager.list_files(folder)
+    if is_htmx:
+        template = "components/file_list.html"
+    else:
+        template = "files.html"
     return templates.TemplateResponse(
         request,
-        "files.html",
+        template,
         {"hanko_url": HANKO_URL, "user": user, "files": files, "folder": folder},
     )
