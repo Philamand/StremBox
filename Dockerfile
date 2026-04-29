ffd0869fdfe96d88e86f6cd8e7132b9ec2f03c03
Philamand <contact@autobacklog.com>
2026-04-29 10:23:40 +0200

    Add docker compose

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.14-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+ENV PATH="/app/venv/bin:$PATH"
+
+WORKDIR /app
+
+RUN python -m venv /app/venv
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-QBT_HOST = os.getenv("QBT_HOST", "localhost")
+QBT_HOST = os.getenv("QBT_HOST", "gluetun")
 QBT_PORT = int(os.getenv("QBT_PORT", "8080"))
 QBT_USERNAME = os.getenv("QBT_USERNAME", "admin")
-QBT_PASSWORD = os.getenv("QBT_PASSWORD", "password")
+QBT_PASSWORD = os.getenv("QBT_PASSWORD", "adminadmin")
 
 BASE_DIR = os.getenv("BASE_DIR", "./data/")
 
