8f652b30f8f848104de534a4a521414248b5d374
Philamand <philamand@protonmail.com>
2026-05-10 10:58:32 +0200

    Add transmission and users table create migrations

--- db/migrations/20260510084639_create_transmission_table.sql
+++ db/migrations/20260510084639_create_transmission_table.sql
@@ -1,5 +1,10 @@
 -- migrate:up
-
+CREATE TABLE `transmission` (
+  `id` integer not null primary key autoincrement,
+  `created_at` datetime not null default CURRENT_TIMESTAMP,
+  `port` INT not null,
+  `download_folder` varchar(255) not null,
+  unique (`id`)
+)
 
 -- migrate:down
-
--- db/migrations/20260510084643_create_users_table.sql
+++ db/migrations/20260510084643_create_users_table.sql
@@ -1,5 +1,10 @@
 -- migrate:up
-
+CREATE TABLE `users` (
+  `id` VARCHAR(255) not null,
+  `created_at` datetime not null default CURRENT_TIMESTAMP,
+  "transmission_id" INT NULL,
+  primary key (`id`),
+  foreign key (`transmission_id`) references transmission (id) on update cascade on delete cascade
+)
 
 -- migrate:down
-
