53f765e2872898e2a98aec184d1183f7bcf29a67
Philamand <philamand@protonmail.com>
2026-05-10 14:56:04 +0200

    Fix missing volume in docker compose

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ BEARER_TOKEN = os.getenv("BEARER_TOKEN", "test_token")
 HANKO_URL = os.getenv("HANKO_URL", "http://localhost:8000")
 HANKO_ADMIN = os.getenv("HANKO_ADMIN", "")
 
-DATABASE_URL = os.getenv("DATABASE_URL", "database.db")
+DATABASE_URL = os.getenv("DATABASE_URL_TEST", "database/database.db")
 
 TRANSMISSION_URL = os.getenv("TRANSMISSION_URL", "gluetun")
