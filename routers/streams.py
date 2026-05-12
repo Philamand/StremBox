c6e95b7c78fd96ab3b3a2ed1806c8fbaf5f8a2c4
Philamand <philamand@protonmail.com>
2026-05-12 16:28:16 +0200

    Add download route

--- routers/streams.py
+++ routers/streams.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import FileResponse
 
 from services.files import FileManager
+from services.torrents import TorrentService
 from utils.security import check_user_key
 
 router = APIRouter(prefix="/streams", dependencies=[Depends(check_user_key)])
@@ -22,3 +23,31 @@ async def get_stream(
         raise HTTPException(status_code=404, detail="File not found")
 
     return FileResponse(path)
+
+
+@router.get("/download/{user_key}/{torrent_hash}")
+async def download_stream(
+    request: Request,
+    torrent_service: Annotated[TorrentService, Depends()],
+    file_service: Annotated[FileManager, Depends()],
+    torrent_hash: str,
+    tracker: str,
+    api_key: str,
+):
+    if tracker == "c411":
+        torrent_url = f"https://c411.org/api?t=get&id={torrent_hash}&apikey={api_key}"
+    else:
+        raise HTTPException(status_code=400, detail="Invalid tracker")
+
+    available_size = (
+        request.state.user.transmission_data.size * 1024 * 1024 * 1024
+        - await file_service.get_folder_size()
+    )
+
+    try:
+        # TODO: Handle redirect to file stream
+        torrent = await torrent_service.add_torrent(
+            torrent=torrent_url, max_size=available_size
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
