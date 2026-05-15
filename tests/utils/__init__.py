6a1403bdca7b685404e3c50fc9af426df0e33b46
Philamand <philamand@protonmail.com>
2026-05-15 10:11:51 +0200

    Remove outdated tests

--- tests/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# Package initializer for tests.utils
-# This file makes `tests.utils` a python package so test discovery/imports work reliably.
-# Keep this file minimal to avoid side effects during test collection.
-
-__all__ = []
