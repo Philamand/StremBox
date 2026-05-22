dba2ab09fe797a82bfbcb4961991a87a60cffabf
Philamand <philamand@protonmail.com>
2026-05-22 14:33:21 +0200

    Add download_stream_head route

--- streams/route.py
+++ streams/route.py
@@ -143,3 +143,70 @@ async def download_stream(
         headers=headers,
         media_type=headers["content-type"],
     )
+
+
+@router.head("/download/{user_key}/{torrent_hash}")
+async def download_stream_head(
+    request: Request,
+    torrent_service: Annotated[TorrentService, Depends()],
+    file_service: Annotated[FileManager, Depends()],
+    torrent_hash: str,
+    tracker: str,
+    api_key: str,
+    torrent_id: Optional[int] = None,
+    season: Optional[int] = None,
+    episode: Optional[int] = None,
+):
+    torrent = await torrent_service.get_torrent(torrent_hash)
+
+    if not torrent:
+        if tracker == "c411":
+            torrent_url = (
+                f"https://c411.org/api?t=get&id={torrent_hash}&apikey={api_key}"
+            )
+        elif tracker == "torr9" and torrent_id:
+            torrent_url = f"https://api.torr9.net/api/v1/rss/torrents/{torrent_id}/download?passkey={api_key}"
+        else:
+            raise HTTPException(status_code=400, detail="Invalid tracker")
+
+        available_size = (
+            request.state.user.transmission_data.size * 1024 * 1024 * 1024
+            - await file_service.get_folder_size()
+        )
+
+        try:
+            added_hash = await torrent_service.add_torrent(
+                torrent=torrent_url, max_size=available_size
+            )
+            await torrent_service.wait_for_download_start(
+                added_hash, timeout=30.0, min_percent=0.01
+            )
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+    torrent_files = await torrent_service.get_torrent_files(torrent_hash)
+    file_path = resolve_file_path(
+        torrent_files[0].name,
+        request.state.user.transmission_data.download_folder,
+        season,
+        episode,
+    )
+
+    file_range = request.headers.get("range")
+
+    path, start, end, file_size = await build_stream_response(
+        file_service, file_path, file_range
+    )
+
+    try:
+        headers, status_code = build_stream_headers(
+            path, file_range, start, end, file_size
+        )
+    except ValueError:
+        raise HTTPException(status_code=415, detail="Fichier non pris en charge")
+
+    return Response(
+        status_code=status_code,
+        headers=headers,
+        media_type=headers["content-type"],
+    )
