1d3e777cee4e3563c03fa9380609fc7e953d188d
Philamand <philamand@protonmail.com>
2026-06-01 15:05:51 +0200

    Update 20260601083102_create_zip_directory.sql

--- db/migrations/20260601083102_create_zip_directory.sql
+++ db/migrations/20260601083102_create_zip_directory.sql
@@ -2,7 +2,7 @@
 CREATE TABLE `zip_directory` (
   `id` integer not null primary key autoincrement,
   `created_at` datetime not null default CURRENT_TIMESTAMP,
-  `done` BOOLEAN not null,
+  `done` BOOLEAN not null default false,
   `path` varchar(255) not null,
   unique (`id`)
 );
