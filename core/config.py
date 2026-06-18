fddb09842654fc98de2a4425cbac38c5b0d33d19
Philamand <philamand@protonmail.com>
2026-06-18 14:11:45 +0200

    Update class-based Config to model_config dict

--- core/config.py
+++ core/config.py
@@ -1,8 +1,14 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     """Application configuration using Pydantic Settings."""
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_prefix="",
+        extra="ignore",
+    )
 
     base_dir: str = "./downloads/"
     base_url: str = "http://localhost"
@@ -12,11 +18,6 @@ class Settings(BaseSettings):
     fastapi_database_url: str = "database/database.db"
     transmission_url: str = "gluetun"
     origin_url: str = "http://localhost:8000"
-
-    class Config:
-        env_file = ".env"
-        env_prefix = ""
-        extra = "ignore"
 
 
 settings = Settings()
