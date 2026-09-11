0372b26eac80c64a30f89c6e2bcfedc5d7c8ac88
Philamand <philamand@protonmail.com>
2026-09-11 11:56:32 +0200

    Use show slug to find next episode Remove print statement

--- cron_job.py
+++ cron_job.py
@@ -92,10 +92,8 @@ async def main():
 
                     for show in unfinished_shows:
                         next_episode = await trakt_service.get_next_episode(
-                            show.show.ids.imdb, user.trakt_slug
+                            show.show.ids.slug, user.trakt_slug
                         )
-
-                        print(show.show.title, next_episode)
 
                         if next_episode:
                             results = await stremio_service.search_serie(
